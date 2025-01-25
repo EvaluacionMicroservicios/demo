3d2aee83fc9f40d5bb3ea1f8e6f8de940c7e6ca1
Ronnie Gonzales <ronnie_cepafs@hotmail.com>
2025-01-25 04:41:07 -0500

    Primer commit

---
+++ src/main/java/com/demo/demomicroservicio/DemomicroservicioApplication.java
@@ -0,0 +1,13 @@
+package com.demo.demomicroservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomicroservicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/demomicroservicio/DemomicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.demomicroservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
